3dbcf789f1bbab2e67815365c031adc37bc89a3b
sandobits <sandersonbarbosa36@gmail.com>
2020-05-10 03:24:16 -0300

    added user.destroy() & user.view()

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,24 @@
 import { Router } from 'express'
 import * as UserController from './controllers/UserController'
-const router = Router()
+import * as LoginController from './controllers/LoginController'
+
+import checkUserAuth from './middlewares/checkUserAuth'
 
 import * as swaggerDocument from '../swagger.json'
 import * as swaggerUi from 'swagger-ui-express'
 
+const router = Router()
+
 router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 router.get('/', async function (req, res) {
-    return res.status(200).send("HOME\n")
+    return res.status(200).json("HOME")
 })
 
 router.post('/register', UserController.create)
+router.get('/users/:id', checkUserAuth, UserController.view)
+router.delete('/users/:id', checkUserAuth, UserController.destroy)
+
+router.post('/login', LoginController.authorizeUser)
 
 export default router

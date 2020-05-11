b994ef070044dc6f08d0844a97364091e35750b7
sandobits <sandersonbarbosa36@gmail.com>
2020-05-10 22:11:40 -0300

    Added find locale workflow

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import * as UserController from './controllers/UserController'
 import * as LoginController from './controllers/LoginController'
 import * as BusinessController from './controllers/BusinessController'
+import * as LocaleController from './controllers/LocaleController'
 
 import checkUserAuth from './middlewares/checkUserAuth'
 
@@ -21,8 +22,14 @@ router.get('/users/:id', checkUserAuth, UserController.view)
 router.delete('/users/:id', checkUserAuth, UserController.destroy)
 
 router.post('/business/new', checkUserAuth, BusinessController.create)
+
 router.get('/users/:id/businesses', checkUserAuth, BusinessController.getByUser)
+router.get('/:city/neighborhoods', LocaleController.getNeighborhoods)
+
 router.get('/:city/:neighborhood', BusinessController.getByNeighborhood)
+
+router.get('/cities', LocaleController.getCities)
+
 router.post('/login', LoginController.authorizeUser)
 
 export default router
